f659174b3adf2391c9a71e82664f966814bcdc63
BrayanSGL <bsgaleanol@udistrital.edu.co>
2024-08-29 20:25:57 -0500

    chore: Add footer and header templates for views

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//Motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/public")); //Para acceder a los archivos estáticos
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
